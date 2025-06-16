78882d6f7ca55b03a9cddb5305f99126f5fc737b
Anh Tu <sweantu@gmail.com>
2025-06-16 20:16:04 +0700

    feat: update airbnb variables

--- terraform_practice/variables.tf
+++ terraform_practice/variables.tf
@@ -1,32 +1,32 @@
 variable "credentials" {
   description = "The path to the GCP credentials file"
-  default = "~/.gcp/terraform-practice.json"
+  default = "~/.gcp/airbnb-de-zoomcamp-2f68e8307ebc.json"
 }
 
 variable "project" {
   description = "The project ID to deploy resources to"
-  default = "de-zoomcamp-practice-451708"
+  default = "airbnb-de-zoomcamp"
 }
 
 variable "region" {
   description = "The region to deploy resources to"
-  default = "asia-southeast1-b"
+  default = "us-east1"
 }
 
 variable "bucket_name" {
   description = "The name of the bucket to deploy resources to"
-  default = "de-zoomcamp-practice-451708-terraform-bucket"
+  default = "airbnb-de-zoomcamp-bucket"
 }
 
 variable "location" {
   description = "The location of the bucket to deploy resources to"
-  default = "asia"
+  default = "us"
 }
 
 
 variable "dataset_id" {
   description = "The ID of the dataset to deploy resources to"
-  default = "terraform_dataset"
+  default = "airbnb_de_zoomcamp_dataset"
 }
 
 
